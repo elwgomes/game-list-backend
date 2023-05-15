f8f36b2913e57494215812e18ab179f723a0dade
Leonardo <elwgomes@icloud.com>
2023-05-15 13:48:45 -0300

    initial commit

---
+++ src/main/java/com/elwgomes/dslist/DslistApplication.java
@@ -0,0 +1,13 @@
+package com.elwgomes.dslist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DslistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DslistApplication.class, args);
+		System.out.println("Application is running on...");
+	}
+}
